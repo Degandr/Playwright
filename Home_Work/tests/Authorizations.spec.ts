9b0033d646a2c4afd08af43c8c09df34dc8e2ed5
Degand <freenick96@gmail.com>
2022-12-24 16:06:58 +0100

    Adding Log4JS

--- Home_Work/tests/Authorizations.spec.ts
+++ Home_Work/tests/Authorizations.spec.ts
@@ -1,5 +1,6 @@
 import { test } from "@playwright/test";
 import { Login_Page } from "../Pages/page_login";
+import { logger } from "../../configLogger";
 
 test.describe("Authorizations", () => {
   test("Valid authorization", async ({ page }) => {
@@ -14,11 +15,17 @@ test.describe("Authorizations", () => {
   });
 
   test("Invalid authorization", async ({ page }) => {
+    logger.debug("Exemplar creation");
     const loginPage = new Login_Page(page);
+    logger.debug("Go to the Login page");
     await loginPage.goto();
+    logger.debug("Fill Login filed");
     await loginPage.fillLogin("tomsmith1");
+    logger.debug("Fill Password filed");
     await loginPage.fillPassword("SuperSecretPassword!1");
+    logger.debug("Click Login button");
     await loginPage.clickLoginButton();
+    logger.debug("Get Error message");
     await loginPage.flashNotOK(/Your username is invalid!.*/);
   });
 });
---
+++ configLogger.ts
@@ -0,0 +1,34 @@
+import log4js from "log4js";
+const dateFormat = "%d{[dd/MM/yyyy] [hh:mm:ss]} [%p] - %m";
+
+log4js.configure({
+  appenders: {
+    fullLog: {
+      type: "file",
+      filename: "./logs/fullLog.log",
+      layout: {
+        type: "pattern",
+        pattern: dateFormat,
+      },
+    },
+    errorLog: {
+      type: "file",
+      filename: "./logs/fullLog.log",
+      layout: {
+        type: "pattern",
+        pattern: dateFormat,
+      },
+    },
+    "just-errors": { type: "logLevelFilter", appender: "errorLog", level: "error" },
+    console: {
+      type: "console",
+      layout: { type: "basic" },
+    },
+    "just-errors-console": { type: "logLevelFilter", appender: "console", level: "error" },
+  },
+  categories: {
+    default: { appenders: ["fullLog", "just-errors", "just-errors-console"], level: "debug" },
+  },
+});
+
+export const logger = log4js.getLogger();
